942c604918ea26f9cdada909c750def37ef0fca1
Christian Hapke <chris.github@hapke.com>
2024-09-03 16:57:22 +0200

    Fine tuning parameter names in llm_factory.py

--- factory/llm_factory.py
+++ factory/llm_factory.py
@@ -9,6 +9,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+#
+# Specific LLM instances and their setup
+#
 
 @cache
 def get_default_llm_without_streaming() -> BaseChatModel:
@@ -25,36 +28,39 @@ def get_default_llm_with_streaming() -> BaseChatModel:
 
 
 #
-# helper functions for dynamic LLM setup
+# Helper functions for dynamic LLM setup
 #
 
-def setup_llm_from_config(llm_full_config: Optional[Dict]) -> BaseChatModel:
+def setup_llm_from_config(llm_config: Optional[Dict]) -> Optional[BaseChatModel]:
     """
     Setup LLM from full config. Fails with error if not possible.
 
     Args:
-        llm_full_config (dict): LLM full configuration
+        llm_config (dict): LLM full configuration
         
     Returns:
-        BaseChatModel: LLM instance
+        BaseChatModel: LLM instance, None in the case of an error
     """
-    # pre-checks
-    if llm_full_config is None:
+    # Pre-checks
+    if llm_config is None:
         logger.error(f"Error in LLM setup: llm_long_config is None")
         return None
-    llm_name = llm_full_config["name"]
+    llm_name = llm_config["name"]
     if llm_name is None:
         logger.error(f"Error in LLM setup: LLM 'name' is not configured")
         return None
-    llm_config = llm_full_config[llm_name]
+    llm_config = llm_config[llm_name]
     if llm_config is None:
         logger.error(f"Error in LLM setup: no config found for LLM name='{llm_name}'")
         return None
 
-    # action
-    return setup_llm_from_specific_config(llm_config, llm_name)
+    # Action
+    return setup_llm_for_single_config(llm_config, llm_name)
 
-def setup_llm_from_specific_config(llm_config: Optional[Dict], llm_name: Optional[str]) -> BaseChatModel:
+def setup_llm_for_single_config(
+        llm_config: Optional[Dict],
+        llm_name: Optional[str]
+    ) -> BaseChatModel:
     """
     Setup LLM from config. Fails with error if not possible.
 
@@ -63,35 +69,34 @@ def setup_llm_from_specific_config(llm_config: Optional[Dict], llm_name: Optiona
         llm_name (str): LLM name (for logging purposes only)
         
     Returns:
-        BaseChatModel: LLM instance
+        BaseChatModel: LLM instance, None in the case of an error
     """
     try:
         # TODO: remove access tokens before logging
         logger.info(f"Setup LLM: {llm_name}({llm_config})")
 
-        # pre-checks
+        # Pre-checks
         if llm_config is None:
             logger.error(f"Error in LLM setup: llm_config is None for {llm_name}")
             return None
 
-        # get the the parameters
+        # Get the the parameters
         module_name = llm_config["module"]
         class_name = llm_config["class"]
         class_kwargs = llm_config["args"]
 
-        # instantiate the class (dynamic instantiation)
-        #instance = globals()[class_name](**class_kwargs)
-        #instance = langchain_openai.ChatOpenAI(model_name="gpt-4o", temperature=0, streaming=True)
+        # Instantiate the class (dynamic instantiation)
         import importlib
         module = importlib.import_module(module_name)
         class_ = getattr(module, class_name)
         instance = class_(**class_kwargs)
+        # Incomplete(!!!) alternavive would be:
+        #   instance = globals()[class_name](**class_kwargs)
+        #   instance = langchain_openai.ChatOpenAI(model_name="gpt-4o", temperature=0, streaming=True)
 
         return instance
 
     except Exception as e:
         logger.debug(f"Error in setup LLM: {llm_name}({class_kwargs}): {e}", e)
         logger.error(f"Error in setup LLM: {llm_name}({class_kwargs}): {e}")
-        exit(1)
-
-
+        return None
