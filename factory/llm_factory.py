54c73a454129e3f6c58f9e4dfa39c114248b4fd9
Christian Hapke <chris.github@hapke.com>
2024-09-03 22:26:57 +0200

    Reorganized LLM-configuration in yaml

--- factory/llm_factory.py
+++ factory/llm_factory.py
@@ -15,29 +15,29 @@ logger = logging.getLogger(__name__)
 
 @cache
 def get_default_llm_without_streaming() -> BaseChatModel:
-    llm_full_config = settings.config.common["default_llm"]
-    llm = setup_llm_from_config(llm_full_config)
-    logger.info(f"Setup done: config.common.default_llm={llm}")
+    config_llm_key = settings.config.rag_response_service["default_llm"]
+    llm = setup_llm_for_config_llm_key(config_llm_key)
+    logger.info(f"Setup done: config.rag_response_service.default_llm={llm}")
     return llm
 @cache
 def get_default_llm_with_streaming() -> BaseChatModel:
-    llm_full_config = settings.config.common["default_llm_with_streaming"]
-    llm = setup_llm_from_config(llm_full_config)
-    logger.info(f"Setup done: config.common.default_llm_with_streaming={llm}")
+    config_llm_key = settings.config.rag_response_service["default_llm_with_streaming"]
+    llm = setup_llm_for_config_llm_key(config_llm_key)
+    logger.info(f"Setup done: config.rag_response_service.default_llm_with_streaming={llm}")
     return llm
 
 
 def get_document_grader_llm() -> BaseChatModel:
-    llm_full_config = settings.config.common["document_grader_llm"]
-    llm = setup_llm_from_config(llm_full_config)
-    logger.info(f"Setup done: config.common.document_grader_llm={llm}")
+    config_llm_key = settings.config.rag_response_service["document_grader_llm"]
+    llm = setup_llm_for_config_llm_key(config_llm_key)
+    logger.info(f"Setup done: config.rag_response_service.document_grader_llm={llm}")
     return llm
 
 
 def get_rewrite_question_llm() -> BaseChatModel:
-    llm_full_config = settings.config.common["rewrite_question_llm"]
-    llm = setup_llm_from_config(llm_full_config)
-    logger.info(f"Setup done: config.common.rewrite_question_llm={llm}")
+    config_llm_key = settings.config.rag_response_service["rewrite_question_llm"]
+    llm = setup_llm_for_config_llm_key(config_llm_key)
+    logger.info(f"Setup done: config.rag_response_service.rewrite_question_llm={llm}")
     return llm
 
 
@@ -45,53 +45,32 @@ def get_rewrite_question_llm() -> BaseChatModel:
 # Helper functions for dynamic LLM setup
 #
 
-def setup_llm_from_config(llm_config: Optional[Dict]) -> Optional[BaseChatModel]:
-    """
-    Setup LLM from full config. Fails with error if not possible.
+def setup_llm_for_config_llm_key(config_llm_key: str) -> Optional[BaseChatModel]:
+    logger.info(f"Setup LLM from config_llm_key: {config_llm_key}")
+    llm_config = settings.config.common.llms[config_llm_key]
+    return setup_llm_for_config(config_llm_key=config_llm_key, llm_config=llm_config)
 
-    Args:
-        llm_config (dict): LLM full configuration
-        
-    Returns:
-        BaseChatModel: LLM instance, None in the case of an error
-    """
-    # Pre-checks
-    if llm_config is None:
-        logger.error(f"Error in LLM setup: llm_long_config is None")
-        return None
-    llm_name = llm_config["name"]
-    if llm_name is None:
-        logger.error(f"Error in LLM setup: LLM 'name' is not configured")
-        return None
-    llm_config = llm_config[llm_name]
-    if llm_config is None:
-        logger.error(f"Error in LLM setup: no config found for LLM name='{llm_name}'")
-        return None
-
-    # Action
-    return setup_llm_for_single_config(llm_config, llm_name)
-
-def setup_llm_for_single_config(
+def setup_llm_for_config(
+        config_llm_key: Optional[str],
         llm_config: Optional[Dict],
-        llm_name: Optional[str]
     ) -> BaseChatModel:
     """
     Setup LLM from config. Fails with error if not possible.
 
     Args:
+        config_llm_key (str): LLM name (for logging purposes only)
         llm_config (dict): LLM configuration
-        llm_name (str): LLM name (for logging purposes only)
         
     Returns:
         BaseChatModel: LLM instance, None in the case of an error
     """
     try:
         # TODO: remove access tokens before logging
-        logger.info(f"Setup LLM: {llm_name}({llm_config})")
+        logger.info(f"Setup LLM: {config_llm_key}({llm_config})")
 
         # Pre-checks
         if llm_config is None:
-            logger.error(f"Error in LLM setup: llm_config is None for {llm_name}")
+            logger.error(f"Error in LLM setup: llm_config is None for {config_llm_key}")
             return None
 
         # Get the the parameters
@@ -111,6 +90,6 @@ def setup_llm_for_single_config(
         return instance
 
     except Exception as e:
-        logger.debug(f"Error in setup LLM: {llm_name}({class_kwargs}): {e}", e)
-        logger.error(f"Error in setup LLM: {llm_name}({class_kwargs}): {e}")
+        logger.debug(f"Error in setup LLM: {config_llm_key}({class_kwargs}): {e}", e)
+        logger.error(f"Error in setup LLM: {config_llm_key}({class_kwargs}): {e}")
         return None
